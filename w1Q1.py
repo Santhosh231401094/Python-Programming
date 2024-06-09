02f08d53815befea0afa699a45717b70614928f9
Santhosh231401094 <231401094@rajalakshmi.edu.in>
2024-06-09 13:36:43 +0530

    w1Q1.py

--- w1Q1.py
+++ w1Q1.py
@@ -1,17 +1,3 @@
-"""Write a program to convert strings to an integer and float and display its type.
-
-Sample Input:
-
-10
-
-10.9
-
-Sample Output:
-
-10,<class 'int'>
-
-10.9,<class 'float'>"""
-
 a=input()
 b=input()
 c=int(a)
